4b7694f181a9722b2dfdb09e101bd990812c1077
Naomi 💜 <naomiblack@Naomis-MacBook-Air.local>
2017-07-25 21:15:13 -0400

    update

--- script.js
+++ script.js
@@ -1,41 +1,30 @@
 
 //gorceryList objects in array
 var groceryList = [
-  {
-name:"flour",
-price:5
-},
-{
-  name:"sugar",
-  price:3.50
-},
-{
-  name:"yeast",
-  price:.98
-}
-];
+                    {name:"flour", price:5},
+                    {name:"sugar", price:3.50},
+                    {name:"yeast",price:.98} ];
 
 
 
 var total=0;
 
 //loop through function to add items to groceryReceipt
-groceryList.forEach(function(item){
+groceryList.forEach(function(item) {
   //create list element
   var itemName= document.createElement("li");
-itemName.innerHTML=item.name + " &#36; " + item.price.toFixed(2);
-document.body.appendChild(itemName);
-total+=item.price
+  itemName.innerHTML=item.name + " &#36; " + item.price.toFixed(2);
+  document.body.appendChild(itemName);
+
 
 
 });
 
-//total+= groceryList.price;
-for(var i=0; i< groceryList.length; i++){
-var subtotal= document.createElement("p")
-subtotal.innerHTML= "Your subtotal is "+ "$" + total
-document.body.appendChild(subtotal);
 
-    //total+=item.price;
+for(var i=0; i < groceryList.length; i++){
+  total += groceryList[i].price;
+
 }
-  
+var subtotal= document.createElement("p")
+subtotal.innerHTML= "Your total is "+ "$" + total
+document.body.appendChild(subtotal);
